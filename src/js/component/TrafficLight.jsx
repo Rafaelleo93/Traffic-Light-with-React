fbfcd6df5153e14203c641a3b96a0bd3a3dff8cb
Rafaelleo93 <rafaelleonardo93@gmail.com>
2022-08-15 16:32:35 +0000

    Solución errores

--- src/js/component/TrafficLight.jsx
+++ src/js/component/TrafficLight.jsx
@@ -1,13 +1,22 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
-const TrafficLight = () => {
+const TrafficLight = (props) => {
   const [color, setColor] = useState();
+
   return (
-    <div className='light-container d-flex  flex-column justify-content-center align-items-center'>
-      <div className={color === 'red' ? 'light red selected' : 'light red'} onClick={() => setColor ('red')}></div>
-      <div className={color === 'yellow' ? 'light yellow selected' : 'light yellow'} onClick={() => setColor ('yellow')}></div>
-      <div className={color === 'green' ? 'light green selected' : 'light green'} onClick={() => setColor ('green')}></div>
+    <div className='d-flex flex-column align-items-center justify-content-center '>
+      <div className='cable'></div>
+      <div className='light-container d-flex flex-column justify-content-center align-items-center'>
+        <div className={color === 'red' ? 'light red selected' : 'light red'} onClick={() => setColor ('red')}></div>
+        <div className={color === 'yellow' ? 'light yellow selected' : 'light yellow'} onClick={() => setColor ('yellow')}></div>
+        <div className={color === 'green' ? 'light green selected' : 'light green'} onClick={() => setColor ('green')}></div>
+        <div className={color === 'purple' ? 'light purple selected' : 'light purple'} onClick={() => setColor ('purple')}>
+        </div>
+        
+      </div>
+
     </div>
+
   )
 }
 
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,11 +4,10 @@ import TraficLight from "./TrafficLight.jsx";
 //create your first component
 const Home = () => {
 	return (
-		<div className='d-flex align-items-center justify-content-center'>
-				<TraficLight />
-
-	
+		<div>
+			<TraficLight />
 		</div>
+		
 	);
 };
 
